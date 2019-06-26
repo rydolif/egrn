34b0396510062076fdcea7cf00ea4549340fe3e5
rudolif <rudolifrudolif@gmail.com>
2019-06-26 18:27:17 +0200

    pravka fansyubox commit

--- gulpfile.js
+++ gulpfile.js
@@ -51,8 +51,8 @@ gulp.task('styles', function() {
 gulp.task('minjs', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',  			//----jquery
-		'app/libs/jquery.validate.js', 					//----форма
-		'app/libs/jquery.mask.min.js', 					//----форма
+		// 'app/libs/jquery.validate.js', 					//----форма
+		// 'app/libs/jquery.mask.min.js', 					//----форма
 		//'app/libs/jquery.popupoverlay.js', 				//----модалки
 		// 'app/libs/slick/slick.js', 					//----слайдер
 		 // 'app/libs/flipclock/flipclock.js',
@@ -60,7 +60,7 @@ gulp.task('minjs', function() {
 		'app/libs/fancybox/jquery.fancybox.js', 		//----картінка прикліку
 		// 'app/libs/jquery.spincrement.min.js', 		//----цифри анімованні
 		// 'app/libs/isotope.pkgd.min.js', 				//----сетка елементов + фильтр
-		'app/libs/lazy-line-painter-1.9.3.min.js',
+		// 'app/libs/lazy-line-painter-1.9.3.min.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(plumber())
@@ -74,8 +74,8 @@ gulp.task('minjs', function() {
 gulp.task('js', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',  			//----jquery
-		'app/libs/jquery.validate.js', 					//----форма
-		'app/libs/jquery.mask.min.js', 					//----форма
+		// 'app/libs/jquery.validate.js', 					//----форма
+		// 'app/libs/jquery.mask.min.js', 					//----форма
 		//'app/libs/jquery.popupoverlay.js', 				//----модалки
 		// 'app/libs/slick/slick.js', 					//----слайдер
 		 // 'app/libs/flipclock/flipclock.js',			//----таймер
@@ -83,7 +83,7 @@ gulp.task('js', function() {
 		'app/libs/fancybox/jquery.fancybox.js', 		//----картінка прикліку
 		// 'app/libs/jquery.spincrement.min.js', 		//----цифри анімованні
 		// 'app/libs/isotope.pkgd.min.js', 				//----сетка елементов + фильтр
-		'app/libs/lazy-line-painter-1.9.3.min.js',
+		// 'app/libs/lazy-line-painter-1.9.3.min.js',
 		'app/js/common.js', // Always at the end
 	])
 	.pipe(plumber())
